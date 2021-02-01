8e5eed5d2d7215459fe4c1299cb08764e3a8980f
michal-duszak <drduszak@gmail.com>
2021-02-02 00:29:50 +0100

    fix categories names

--- src/js/add-book.js
+++ src/js/add-book.js
@@ -2,8 +2,9 @@ const title = document.querySelector(".form__title");
 const author = document.querySelector(".form__author");
 const category = document.querySelector(".form__category");
 const priority = document.querySelector(".form__priority");
-const form = document.querySelector(".form")
+const form = document.querySelector(".form");
 export const addBook = () => {
+
 const newBook = {
     title: title.value,
     author: author.value,

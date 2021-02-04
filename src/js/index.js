bbe25b976dc427e69e90bfcfb71e7c415b5975bf
Michal Duszak <drduszak@gmail.com>
2021-02-04 01:56:33 +0100

    localstorage fix

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,16 @@ import * as mdb from 'mdb-ui-kit';
 import { initialData as defaultData } from '../data/initialData';
 import Bookshelf from './modules/bookshelf';
 import Bookform from './modules/bookform';
+
 const form = document.querySelector('.form');
-///INIT
+/// INIT
 let initialData;
-if (!localStorage.getItem("data")) {
+if (!localStorage.getItem('data')) {
   initialData = defaultData;
 } else {
-  initialData = JSON.parse()
+  initialData = JSON.parse(localStorage.getItem('data'));
 }
 // initialData = defaultData
-console.log(initialData)
 const bookshelf = new Bookshelf(initialData);
 const bookform = new Bookform();
 bookshelf.insertBooks();
@@ -19,7 +19,6 @@ bookshelf.insertBooks();
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   bookshelf.addBook(bookform.getNewBookData());
-  console.log(bookshelf.books);
   form.reset();
 });
 
--- src/js/modules/bookform.js
+++ src/js/modules/bookform.js
@@ -1,22 +1,31 @@
 export default class Bookform {
-  constructor() {    
-    this.title;
-    this.author;
-    this.category;
-    this.priority;
+  constructor() {
+    this.title = '';
+    this.author = '';
+    this.category = '';
+    this.priority = '';
     this.id = 10;
-    this.tableBody = document.querySelector('.table-body')
+    this.tableBody = document.querySelector('.table-body');
   }
-  getNewBookData() {
-      const newBook = {};
-      this.id = this.id + 1;
-      newBook._id = this.id; 
-      newBook.title = document.querySelector('.form__title').value;
-      newBook.author = document.querySelector('.form__author').value;
-      newBook.category = document.querySelector('.form__category').value;
-      newBook.priority = document.querySelector('.form__priority').value;
-      console.log(newBook)
 
-      return newBook;
+  setLatestId() {
+    if (!localStorage.getItem('latest-id')) {
+      localStorage.setItem('latest-id', 10);
+    } else {
+      this.id = JSON.parse(localStorage.getItem('latest-id'));
+      this.id += 1;
+      localStorage.setItem('latest-id', this.id);
+    }
+  }
+
+  getNewBookData() {
+    const newBook = {};
+    this.setLatestId();
+    newBook._id = this.id;
+    newBook.title = document.querySelector('.form__title').value;
+    newBook.author = document.querySelector('.form__author').value;
+    newBook.category = document.querySelector('.form__category').value;
+    newBook.priority = document.querySelector('.form__priority').value;
+    return newBook;
   }
 }
